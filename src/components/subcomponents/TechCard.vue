9dfd03be6f1522ec6a1d68aaf4b4e6c8c1072426
Zubaer Rahman <zubaer9898@gmail.com>
2021-04-05 15:20:27 +0100

    mobile optmization for skills

--- src/components/subcomponents/TechCard.vue
+++ src/components/subcomponents/TechCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="card is-shadowless">
       <div class="card-image">
-        <figure class="image is-128x128">
+        <figure class="image is-96x96">
         <img :src="image" alt="Placeholder image">
         </figure>
       </div>
-      <div class="card-footer has-text-white"><strong  class="subtitle is-6 has-text-white">{{name}}</strong></div>
+      <div class="card-footer has-text-white"><strong class="subtitle is-6 has-text-white">{{name}}</strong></div>
   </div>
 </template>
 
@@ -29,6 +29,16 @@ export default {
         height: 180px;
         width: 160px;
         background-color:transparent;
+    }
+
+    @media only screen and (max-width: 767px) {
+        .card{
+            padding: 10px;
+            margin: 15px;
+            height: 130px;
+            width: 100px;
+            background-color:transparent;
+        }
     }
 
     strong{
